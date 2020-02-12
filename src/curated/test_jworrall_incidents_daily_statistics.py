547a0840bb04ba3de1926a578ece85e3a7aa9566
jaworra_dev-at-708280501988 <john.a.worrall@gmail.com>
2020-02-12 16:58:24 +1000

    stats method optimisation work 2

--- src/curated/test_jworrall_incidents_daily_statistics.py
+++ src/curated/test_jworrall_incidents_daily_statistics.py
@@ -1,7 +1,7 @@
 #Feature extraction from available API
 
 #todo: 1) refactor holiday and next 24 hour result
-#      2) cloud wat     
+#      2) set cloud watch  
 
 # Based on location of incident ('In progres') return proximity HERE flow network
 # Save S3 location
@@ -30,7 +30,7 @@ import json
 
 def athena_next_24_hrs_past_24_hrs():
     '''
-    create a rolling 48hour window for dashboard statistics every 24hours
+    create a rolling 48hour window for dashboard statistics every 24hours (set at 1145)
     
     '''
     #set current date 
@@ -42,14 +42,15 @@ def athena_next_24_hrs_past_24_hrs():
     dt_24hours=dt + datetime.timedelta(hours=24) #24 hour previous 
     tomorrows_day_of_week = dt_24hours.strftime('%A')
     
-    print day_of_week
-    print tomorrows_day_of_week
+    sel_stat_1 = 1
+    sel_stat_2 = 2
+    col_wkday = 'weekday'    
+    col_inc = 'incidentcount' 
+    col_hm = 'hhmm_utcplus10' 
     
-    return
-
-
+    #anthena query
     '''
-SELECT weekday, hhmm_utcplus10, 1 as select_order,
+    SELECT weekday, hhmm_utcplus10, 1 as select_order,
     min (incidentcount) as "minimum_incidents",
     approx_percentile(incidentcount, 0.25) as "appx_Q1_incidents",
     AVG (incidentcount) as "average_incidents",
@@ -58,7 +59,7 @@ SELECT weekday, hhmm_utcplus10, 1 as select_order,
     FROM "incidents"."daily_summaries_dashboard" where weekday = 'Monday' 
     GROUP BY weekday,hhmm_utcplus10
     UNION 
-SELECT weekday, hhmm_utcplus10, 2 as select_order,
+    SELECT weekday, hhmm_utcplus10, 2 as select_order,
     min (incidentcount) as "minimum_incidents",
     approx_percentile(incidentcount, 0.25) as "appx_Q1_incidents",
     AVG (incidentcount) as "average_incidents",
@@ -68,10 +69,37 @@ SELECT weekday, hhmm_utcplus10, 2 as select_order,
     GROUP BY weekday,hhmm_utcplus10 Order by select_order,hhmm_utcplus10
     '''
     
+    query = "SELECT %s,%s,%d as select_order, " \
+        "min (%s) as minimum_incidents," \
+        "approx_percentile(%s, 0.25) as appx_Q1_incidents," \
+        "AVG (%s) as average_incidents," \
+        "approx_percentile(%s, 0.75) as appx_Q3_incidents," \
+        "max (%s) as maximum_incidents " \
+        "FROM %s.%s where %s = '%s' " \
+        "GROUP BY %s,%s" \
+        " UNION " \
+        "SELECT %s,%s,%d as select_order, " \
+        "min (%s) as minimum_incidents," \
+        "approx_percentile(%s, 0.25) as appx_Q1_incidents," \
+        "AVG (%s) as average_incidents," \
+        "approx_percentile(%s, 0.75) as appx_Q3_incidents," \
+        "max (%s) as maximum_incidents " \
+        "FROM %s.%s where %s = '%s' " \
+        "GROUP BY %s,%s ORDER BY select_order,%s;"  \
+        % (col_wkday, col_hm,sel_stat_1,col_inc,col_inc,col_inc,col_inc,col_inc,
+        DATABASE, TABLE, col_wkday, day_of_week,col_wkday,col_hm,
+        col_wkday, col_hm,sel_stat_2,col_inc,col_inc,col_inc,col_inc,col_inc,
+        DATABASE, TABLE, col_wkday, day_of_week,col_wkday,col_hm,col_hm) 
     
+    #calls athena query
+    results,query_execution_id = athena_qry(query)
     
+    print results
+    print query_execution_id
     
+    return
 
+    
 
 def athena_typical_day_qry():
     '''
@@ -96,26 +124,6 @@ def athena_typical_day_qry():
     #Need to do qa check at 9ma the following code returns the right day (update lambda test_jworrall_incidents_current_delta line 455)
     #print dt.today().strftime('%A')
 
-    #------------------ run athena queries -----------------------
-    #send to athena and query all csvs
-    #create table if required below athena db
-    '''
-    CREATE EXTERNAL TABLE IF NOT EXISTS historic_incidents_db.daily_summaries_partition(
-      `date` string,
-      `weekday` string,
-      `incidentcount` int,
-      `crashcount` int 
-    ) PARTITIONED BY (
-      `yymmdd_utcplus10` string 
-    )
-    ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
-    WITH SERDEPROPERTIES (
-      'serialization.format' = ',',
-      'field.delim' = ','
-    ) LOCATION 's3://public-test-road/stat/bytime/'
-    TBLPROPERTIES ('has_encrypted_data'='false')
-    '''
-    
     #anthena query
     '''
     # daily stats
@@ -144,15 +152,40 @@ def athena_typical_day_qry():
             "max (%s) as maximum_incidents " \
             "FROM %s.%s where %s = '%s' " \
             "GROUP BY %s,%s ORDER BY %s ASC;" \
-            % (col_wkday, col_hm, col_inc,col_inc,col_inc,col_inc,col_inc, DATABASE, TABLE, col_wkday, day_of_week,col_wkday,col_hm,col_hm) #stat query
+            % (col_wkday, col_hm, col_inc,col_inc,col_inc,col_inc,col_inc,
+            DATABASE, TABLE, col_wkday, day_of_week,col_wkday,col_hm,col_hm) #stat query
     
+    #calls athena query
+    results,query_execution_id = athena_qry(query)
 
+    #using id move csv file to S3 curated bucket for typical weekday and weekend
+    client_s3 = boto3.resource('s3')
+    # Copy anthena query to curated csv for frontend rendering - html,D3
+    client_s3.Object(bucketname_routes,  "stat/typical_" + day_of_week.lower() + ".csv").copy_from(CopySource= filepath_incidents_statistic_write + "qry/"+ query_execution_id + ".csv")
+    
+    # Delete the former object A
+    client_s3.Object(bucketname_routes, "stat/qry/"+ query_execution_id + ".csv").delete()
+    client_s3.Object(bucketname_routes, "stat/qry/"+ query_execution_id + ".csv.metadata").delete()
+    #set lifecycle policy in bucket subfolder - month exipiry (28-32 files)
+    print 'SUCCESSFUL - athena_typical_day_qry'
+    return 
+
+
+def athena_qry(query_string):
+    '''
+    lambda athena interaction
+    requires parmaters- query string (e.g 'Select * from tbl') 
+                        athena tbls config and repo set as global variables e.g (i.e tbl = incidents, repo = s3://bukect/qry)
+    
+    
+    '''
+    
     #query and save to s3
     # athena client 
     client = boto3.client('athena')
     # Execution
     response = client.start_query_execution(
-        QueryString=query,
+        QueryString=query_string,
         QueryExecutionContext={
             'Database': DATABASE
         },
@@ -186,32 +219,40 @@ def athena_typical_day_qry():
         raise Exception('TIME OVER')
  
     # get query results
-    result = client.get_query_results(QueryExecutionId=query_execution_id)
-
-    #using id move csv file to S3 curated bucket for typical weekday and weekend
-    client_s3 = boto3.resource('s3')
-    # Copy anthena query to curated csv for frontend rendering - html,D3
-    client_s3.Object(bucketname_routes,  "stat/typical_" + day_of_week.lower() + ".csv").copy_from(CopySource= filepath_incidents_statistic_write + "qry/"+ query_execution_id + ".csv")
+    athena_result = client.get_query_results(QueryExecutionId=query_execution_id)
     
-    # Delete the former object A
-    client_s3.Object(bucketname_routes, "stat/qry/"+ query_execution_id + ".csv").delete()
-    client_s3.Object(bucketname_routes, "stat/qry/"+ query_execution_id + ".csv.metadata").delete()
-    #set lifecycle policy in bucket subfolder - month exipiry (28-32 files)
-    print 'SUCCESSFUL - athena_typical_day_qry'
-    return 
+    return athena_result,query_execution_id
 
 
-#query 24hour ahead...
-    
 def lambda_handler(event, context):
     
     athena_next_24_hrs_past_24_hrs()
     return
-
+    
     try:
         athena_typical_day_qry()
     except Exception, err:
         print err
+    print 'COMPLETED - lamabda'
     
-    print 'SUCCESSFUL - lamabda'
+    
+    #------------------ above athena queries  run on following table-----------------------
+    #create table if required below athena db *refrence incase tabled is drop
+    '''
+    CREATE EXTERNAL TABLE IF NOT EXISTS historic_incidents_db.daily_summaries_partition(
+      `date` string,
+      `weekday` string,
+      `incidentcount` int,
+      `crashcount` int 
+    ) PARTITIONED BY (
+      `yymmdd_utcplus10` string 
+    )
+    ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
+    WITH SERDEPROPERTIES (
+      'serialization.format' = ',',
+      'field.delim' = ','
+    ) LOCATION 's3://public-test-road/stat/bytime/'
+    TBLPROPERTIES ('has_encrypted_data'='false')
+    '''
+    
     return 
